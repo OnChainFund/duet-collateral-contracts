3133356925283d769a2ea0fc684b9d5649ad164b
LJlkdskdjflsa <liliangjya@gmail.com>
2022-05-13 08:33:56 +0800

    feat: try to manipulate traderJoe swap on local fork

---
+++ scripts/trader-joe-swap.ts
@@ -0,0 +1,41 @@
+import { ethers, network } from "hardhat";
+export async function test() {
+const JOE_ROUTER = "0x60aE616a2155Ee3d9A68541Ba4544862310933d4";
+const UNISWAPV2_ROUTER02_ABI = [
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountIn", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+    ],
+    name: "getAmountsOut",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const WAVAX = "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7";
+const PTP = "0x22d4002028f537599bE9f666d1c4Fa138522f9c8";
+
+const traderJoe = new ethers.Contract(
+  JOE_ROUTER,
+  UNISWAPV2_ROUTER02_ABI,
+  ethers.provider
+);
+
+let amountEthFromContract = await traderJoe.getAmountsOut(
+  1000, // 1 ETH
+  [PTP, WAVAX]
+);
+
+console.log(`1000 PTP = ${amountEthFromContract[1].toString()} WAVAX`);
+}
+
+test()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
