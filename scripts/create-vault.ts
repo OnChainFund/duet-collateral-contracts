5cbdd31718518178d20da6acb61ca403af4d85fc
LJlkdskdjflsa <liliangjya@gmail.com>
2022-05-13 08:52:31 +0800

    feat: move script abi logic into a seperate file

--- scripts/create-vault.ts
+++ scripts/create-vault.ts
@@ -1,30 +1,42 @@
 import { ethers, network } from "hardhat";
-import {
-  developmentChains,
-  VOTING_DELAY,
-  proposalsFile,
-  FUNC,
-  PROPOSAL_DESCRIPTION,
-  NEW_STORE_VALUE,
-} from "../helper-hardhat-config";
-import * as fs from "fs";
-import { moveBlocks } from "../utils/move-blocks";
-
-export async function createVault(
-  args: any[],
-  functionToCall: string,
-  proposalDescription: string
-) {
+export async function createVault() {
   const appController = await ethers.getContract("AppController");
   const feeConf = await ethers.getContract("FeeConf");
   const singleFarmingVault = await ethers.getContract("SingleFarmingVault");
-  const usdc = "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E";
-
+  // 理論上要使用 dytoken 這裡就會用一般的 token 代替
+  const usdcAddress = "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E";
+  const usdt = "0x8dA6A4e8bBfC9D0Casf9c8F1F2169C6c01FD6D068Dasdf"; // 錯誤的合約
+  console.log(typeof (appController.address));
+  console.log(feeConf.address);
+  console.log(singleFarmingVault.address);
   // initialize the vault
-  singleFarmingVault.initialize(appController, feeConf, usdc);
+  await appController.initialize();
+ const usdc = new ethers.Contract(
+   usdcAddress,
+   UNISWAPV2_ROUTER02_ABI,
+   ethers.provider
+ ); 
+  await singleFarmingVault.initialize(
+    appController.address,
+    feeConf.address,
+    usdc
+  );
+  
+  // deposit
+
+  //await singleFarmingVault.deposit(usdc, 10);
+  //await singleFarmingVault.deposit(usdt, 10);// 這行理論上應該要報錯,但沒有
+  // depositTo
+  //singleFarmingVault.depositTo(usdc, 10);
+  //singleFarmingVault.deposit(usdc, 10);
+  //singleFarmingVault.deposit(); //?
+  // syncDeposit
+  // withdraw
+  // withdrawTo
+  // liquidate
 }
 
-createVault([NEW_STORE_VALUE], FUNC, PROPOSAL_DESCRIPTION)
+createVault()
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
