b3930ae849ea0348bce11619b9ff4e9bb33886cf
LJlkdskdjflsa <liliangjya@gmail.com>
2022-05-14 09:05:05 +0800

    feat: deposite wavax

--- note/command.md
+++ note/command.md
@@ -1,6 +1,7 @@
 run script with local network
 ```
 yarn hardhat --network localhost run scripts/create-vault.ts
+yarn hardhat --network localhost run scripts/
 ```
 
 run hardhat local testnet
--- scripts/create-vault.ts
+++ scripts/create-vault.ts
@@ -1,30 +1,46 @@
-import { ethers, network } from "hardhat";
-export async function createVault() {
+import { ethers } from "hardhat";
+import { ERC20_ABI, WAVAX_ABI } from "./abi";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import "@typechain/hardhat";
+import { getBalance } from "./utils";
+import { USDC, WAVAX } from "./const";
+
+const hre = require("hardhat");
+export async function createVault(hre: HardhatRuntimeEnvironment) {
+  // 前處理
+  const accounts = await hre.ethers.getSigners();
+
+  // 獲取內部合約地址
   const appController = await ethers.getContract("AppController");
   const feeConf = await ethers.getContract("FeeConf");
   const singleFarmingVault = await ethers.getContract("SingleFarmingVault");
-  // 理論上要使用 dytoken 這裡就會用一般的 token 代替
-  const usdcAddress = "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E";
-  const usdt = "0x8dA6A4e8bBfC9D0Casf9c8F1F2169C6c01FD6D068Dasdf"; // 錯誤的合約
-  console.log(typeof (appController.address));
-  console.log(feeConf.address);
-  console.log(singleFarmingVault.address);
-  // initialize the vault
-  await appController.initialize();
- const usdc = new ethers.Contract(
-   usdcAddress,
-   UNISWAPV2_ROUTER02_ABI,
-   ethers.provider
- ); 
-  await singleFarmingVault.initialize(
-    appController.address,
-    feeConf.address,
-    usdc
-  );
-  
-  // deposit
 
-  //await singleFarmingVault.deposit(usdc, 10);
+  // 新增外部合約
+
+  // wavax 操作
+  const wavaxContract = new ethers.Contract(WAVAX, WAVAX_ABI, ethers.provider);
+
+  // 理論上要使用 dytoken 這裡就會用一般的 token 代替
+
+  // initialize the vault
+  //await appController.initialize();
+  const usdc = new ethers.Contract(USDC, ERC20_ABI, ethers.provider);
+  /*
+  await singleFarmingVault
+    .connect(accounts[0])
+    .initialize(appController.address, feeConf.address, WAVAX, {
+      gasLimit: 20e4,
+      //gasPrice: 20e14,
+    });
+    */
+  // deposit
+  // 換 wavax
+  await getBalance(wavaxContract, accounts[0].address);
+  await wavaxContract
+    .connect(accounts[0])
+    .deposit({ value: BigInt(1e18), gasPrice: 20e12 });
+  await getBalance(wavaxContract, accounts[0].address);
+  await singleFarmingVault.connect(accounts[0]).deposit(WAVAX, 1);
   //await singleFarmingVault.deposit(usdt, 10);// 這行理論上應該要報錯,但沒有
   // depositTo
   //singleFarmingVault.depositTo(usdc, 10);
@@ -36,7 +52,7 @@ export async function createVault() {
   // liquidate
 }
 
-createVault()
+createVault(hre)
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
