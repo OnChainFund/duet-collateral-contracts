0ffd37037cebc8a85415e204a4eb340c2fae81c3
LJlkdskdjflsa <liliangjya@gmail.com>
2022-05-14 23:09:56 +0800

    feat: change deploy order

--- deploy/04-deploy-dy-token-erc20.ts
+++ deploy/04-deploy-dy-token-erc20.ts
@@ -2,9 +2,9 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import verify from "../helper-functions";
 import { networkConfig, developmentChains } from "../helper-hardhat-config";
+import { WAVAX } from "../utils/const";
 import { ethers } from "hardhat";
-
-const deploySingleFarmingVault: DeployFunction = async function (
+const dyTokenERC20: DeployFunction = async function (
   hre: HardhatRuntimeEnvironment
 ) {
   // @ts-ignore
@@ -12,24 +12,25 @@ const deploySingleFarmingVault: DeployFunction = async function (
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   log("----------------------------------------------------");
-  log("Deploying SingleFarmingVault and waiting for confirmations...");
-  const singleFarmingVault = await deploy("SingleFarmingVault", {
+  log("Deploying DYWAVAX and waiting for confirmations...");
+  const appController = await ethers.getContract("AppController");
+  const lpFarmingVault = await deploy("DYTokenERC20", {
     from: deployer,
-    args: [],
+    args: [WAVAX, "DYWAVAX", appController.address],
     log: true,
     // we need to wait if on a live network so we can verify properly
     waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
   });
-  log(`SingleFarmingVault at ${singleFarmingVault.address}`);
+  log(`DYWAVAX at ${lpFarmingVault.address}`);
   if (
     !developmentChains.includes(network.name) &&
     process.env.ETHERSCAN_API_KEY
   ) {
-    await verify(singleFarmingVault.address, []);
-    log(`Verified contract ${singleFarmingVault.address}`);
+    await verify(lpFarmingVault.address, []);
+    log(`Verified contract ${lpFarmingVault.address}`);
   }
   log(`Delegating to ${deployer}`);
 };
 
-export default deploySingleFarmingVault;
-deploySingleFarmingVault.tags = ["all", "controller"];
+export default dyTokenERC20;
+dyTokenERC20.tags = ["all", "controller"];
